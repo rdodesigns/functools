45fbcaad2dd04703ebcb0d7a6f1ff61b29f17cc9
Ryan Orendorff <ryan@rdodesigns.com>
2015-02-23 18:14:59 -0800

    Fix intersperse missing foldl1 import

--- +functools/+list/intersperse.m
+++ +functools/+list/intersperse.m
@@ -1,4 +1,5 @@
 %INTERSPERSE: Intersperses an input between the elements of a list.
 function out = intersperse(element, list)
+  import functools.list.foldl1
   out = foldl1(@(x,y) (horzcat(x,element,y)), list);
 end
