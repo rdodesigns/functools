621a5e80332581a2c95abcfde0f08b95bfb9803d
Ryan Orendorff <ryan@rdodesigns.com>
2015-02-23 18:15:22 -0800

    Add basic calling documentation to intersperse

--- +functools/+list/intersperse.m
+++ +functools/+list/intersperse.m
@@ -1,4 +1,6 @@
 %INTERSPERSE: Intersperses an input between the elements of a list.
+%
+%   new_list = intersperse(element, list)
 function out = intersperse(element, list)
   import functools.list.foldl1
   out = foldl1(@(x,y) (horzcat(x,element,y)), list);
