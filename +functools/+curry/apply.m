0cec9c572c404b783978705696068560c70d6bc6
Ryan Orendorff <ryan@rdodesigns.com>
2015-02-26 23:29:11 -0800

    Expand apply doc to include CPS and calling syntax
    Hooray for CPS.

--- +functools/+curry/apply.m
+++ +functools/+curry/apply.m
@@ -4,6 +4,18 @@
 %  in higher order functions.
 %
 %  This function is curried, see the calling structure below.
+%
+%  g = apply(f) returns a function g that, when given a value x, performs f(x).
+%  val = apply(f, x) returns f(x)
+%
+%  Note that if flip is used with apply (flip(@apply)) then the result is a
+%  continuation passing style computation.
+%
+%  >> suspended = feval(flip(@apply), 2);
+%  >> suspended(@(x) x^2 + 2)
+%
+%  ans =
+%       6
 
 function out = apply(f, x)
   import functools.curry.apply;
